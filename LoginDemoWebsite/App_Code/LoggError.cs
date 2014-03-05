3426f7e25b2d8e6a10b315c716485a3f39e0b9ce
drashtiparikh <drashti.ce23@gmail.com>
2014-03-05 14:03:26 -0800

    login

---
+++ LoginDemoWebsite/App_Code/LoggError.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for LoggError
+/// </summary>
+public class LoggError
+{
+    /// <summary>
+    /// Writes error occured in log file,if log file does not exist,it creates the file first.
+    /// </summary>
+    /// <param name="exception">Exception</param>
+    public static void Write(Exception exception)
+    {
+        string logFile = String.Empty;
+        StreamWriter logWriter;
+        try
+        {
+            logFile = HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["ErrorLog"].ToString());
+            if (File.Exists(logFile))
+                logWriter = File.AppendText(logFile);
+            else
+                logWriter = File.CreateText(logFile);
+            logWriter.WriteLine("=>" + DateTime.Now + " " + " An Error occured : " +
+                exception.StackTrace + " Message : " + exception.Message + "\n\n");
+            logWriter.Close();
+        }
+        catch (Exception e)
+        {
+            throw;
+        }
+    }
+}
---
+++ LoginDemoWebsite/App_Code/SendMailFuntion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+/// <summary>
+/// Summary description for clsSendMailFuntion
+/// </summary>
+public class SendMailFuntion
+{
+    public SendMailFuntion()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public bool SendMail(string from, string to, string subject, string body)
+    {
+        try
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add(from);
+            mail.From = new MailAddress(to);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            SmtpClient smtpClient = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("project.temp2014@gmail.com", "rakesh@123");
+            smtpClient.Send(mail);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
