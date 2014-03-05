211152752317c804eac21fddc9b3e52e61fd9a64
drashtiparikh <drashti.ce23@gmail.com>
2014-03-05 14:04:06 -0800

    login

---
+++ LoginDemoWebsite/CheckUserData.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LoginBusiness;
+using LoginDataProp;
+
+public partial class CheckUserData : System.Web.UI.Page
+{
+    #region Variable Declaration
+    UserDataBAL userDataBAL;
+    UserData userData;
+    #endregion
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            string querystringId = Convert.ToString(Request.QueryString["id"]);
+            if (querystringId != null)
+            {
+                userDataBAL = new UserDataBAL();
+                userData = new UserData();
+
+                string userId = StringCipher.Decrypt(querystringId, "123");
+
+                userData.Id = Convert.ToInt32(userId);
+                bool resultUpdate = userDataBAL.updateUserData(userData);
+
+                if (resultUpdate == true)
+                {
+                    lblMsgtext.Text = @"Your email id is confirmed in web site. Please <a href=""Login.aspx"" > Login </a> into your account .";
+                    lblMsgtext.CssClass = "msgText";
+                }
+                else
+                {
+                    lblMsgtext.Text = "Your email id is confirmed in web site.";
+                    lblMsgtext.CssClass = "errorText";
+                }
+            }
+        }
+    }
+}
